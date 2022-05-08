c84f75c156b0b9f1efc21c86e3bd7987253cb255
nikitaSkieller <101208939+nikitaSkieller@users.noreply.github.com>
2022-05-08 13:44:50 +0200

    Update test.cpp

--- ArduinoCommunication/test.cpp
+++ ArduinoCommunication/test.cpp
@@ -1,10 +1,17 @@
 #include "SimpleSerial.h"
+#include <conio.h>
 
 int main()
 {
-	SimpleSerial serialCom("COM4", 9600);
+	SimpleSerial serialCom("COM3", 9600);
 
 	cout << serialCom.WriteSerialPort("1") << endl;
+
+	while (1)
+	{
+		char pressedKey = _getch();
+		serialCom.WriteSerialPort(&pressedKey);
+	}
 
 	return 0;
 }
